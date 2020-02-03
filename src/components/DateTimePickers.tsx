e3a6c5e94afd33036cad6646b3d7d3136aa515f6
Andó Botár <andobotar@yahoo.com>
2020-02-03 22:22:32 +0100

    Fix date validation

--- src/components/DateTimePickers.tsx
+++ src/components/DateTimePickers.tsx
@@ -4,17 +4,21 @@ import { FormComponentProps } from 'antd/es/form';
 
 const { RangePicker } = DatePicker;
 
+const dateFormat = 'YYYY-MM-DD';
+const timeFormat = 'HH:mm:ss';
+const dateTimeFormat = dateFormat + ' ' + timeFormat;
+
 const DateTimePickers: React.FC<FormComponentProps> = ({ form }: FormComponentProps) => {
     const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = form;
 
-    // the actual date for validation
-    const minimumDate = new Date();
+    // the current date for validation
+    const today = new Date();
+    const todayISOString = today.toISOString().substring(0, 10);
 
     useEffect(() => {
         form.validateFields();
         // eslint-disable-next-line
     }, []);
-
 
     // Only show error after a field is touched - touched here means 'changed by user' and not 'changed and clicked away'
     const datePickerError = isFieldTouched('datePicker') && getFieldError('datePicker');
@@ -38,12 +42,12 @@ const DateTimePickers: React.FC<FormComponentProps> = ({ form }: FormComponentPr
             const rangeValue = fieldsValue['rangePicker'];
             const values = {
                 ...fieldsValue,
-                'datePicker': fieldsValue['datePicker'].format('YYYY-MM-DD'),
-                'futureDatePicker': fieldsValue['futureDatePicker'].format('YYYY-MM-DD'),
-                'rangePicker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
-                'futureRangePicker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
-                'dateTimePicker': fieldsValue['dateTimePicker'].format('YYYY-MM-DD HH:mm:ss'),
-                'timePicker': fieldsValue['timePicker'].format('HH:mm:ss')
+                datePicker: fieldsValue['datePicker'].format(dateFormat),
+                futureDatePicker: fieldsValue['futureDatePicker'].format(dateFormat),
+                rangePicker: [rangeValue[0].format(dateFormat), rangeValue[1].format(dateFormat)],
+                futureRangePicker: [rangeValue[0].format(dateFormat), rangeValue[1].format(dateFormat)],
+                dateTimePicker: fieldsValue['dateTimePicker'].format(dateTimeFormat),
+                timePicker: fieldsValue['timePicker'].format(timeFormat)
             };
             console.log('Received values of form: ', values);
         });
@@ -55,69 +59,51 @@ const DateTimePickers: React.FC<FormComponentProps> = ({ form }: FormComponentPr
     const timePickerRule = [{ type: 'object', required: true, message: 'Please select time!' }];
 
     return (
-        // form layouts can be 'horizontal' (default), 'vertical' or 'inline'
-        <Form layout="horizontal" onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
             {/* date picker */}
-            <Form.Item label="DatePicker" validateStatus={datePickerError ? 'error' : ''}
-                       help={datePickerError || ''}>
+            <Form.Item label="DatePicker" validateStatus={datePickerError ? 'error' : ''} help={datePickerError || ''}>
                 {getFieldDecorator('datePicker', {
                     rules: datePickerRule
-                })(
-                    <DatePicker/>
-                )}
+                })(<DatePicker />)}
             </Form.Item>
 
-            {/* date picker (no past dates) VALIDÁCIÓT FIXÁLNI!*/}
-            <Form.Item label="DatePicker (past dates are not allowed)"
-                       validateStatus={futureDatePickerError ? 'error' : ''}
-                       help={futureDatePickerError || ''}>
+            {/* date picker (no past dates) */}
+            <Form.Item label="DatePicker (past dates are not allowed)" validateStatus={futureDatePickerError ? 'error' : ''} help={futureDatePickerError || ''}>
                 {getFieldDecorator('futureDatePicker', {
                     rules: datePickerRule
-                })(
-                    <DatePicker disabledDate={d => !d || !d.isAfter(minimumDate)}/>
-                )}
+                })(<DatePicker disabledDate={(d) => !d || !d.isAfter(todayISOString)} />)}
             </Form.Item>
 
             {/* range picker */}
-            <Form.Item label="RangePicker" validateStatus={rangePickerError ? 'error' : ''}
-                       help={rangePickerError || ''}>
+            <Form.Item label="RangePicker" validateStatus={rangePickerError ? 'error' : ''} help={rangePickerError || ''}>
                 {getFieldDecorator('rangePicker', {
                     rules: rangePickerRule
-                })(
-                    <RangePicker/>
-                )}
+                })(<RangePicker />)}
             </Form.Item>
 
-            {/* range picker (no past dates) VALIDÁCIÓT FIXÁLNI!*/}
-            <Form.Item label="RangePicker (past dates are not allowed)"
-                       validateStatus={futureRangePickerError ? 'error' : ''}
-                       help={futureRangePickerError || ''}>
+            {/* range picker (no past dates) */}
+            <Form.Item
+                label="RangePicker (past dates are not allowed)"
+                validateStatus={futureRangePickerError ? 'error' : ''}
+                help={futureRangePickerError || ''}
+            >
                 {getFieldDecorator('futureRangePicker', {
                     rules: rangePickerRule
-                })(
-                    <RangePicker disabledDate={d => !d || !d.isAfter(minimumDate)}/>
-                )}
+                })(<RangePicker disabledDate={(d) => !d || !d.isAfter(todayISOString)} />)}
             </Form.Item>
 
             {/* datetime picker */}
-            <Form.Item label="DateTimePicker"
-                       validateStatus={dateTimePickerError ? 'error' : ''}
-                       help={dateTimePickerError || ''}>
+            <Form.Item label="DateTimePicker" validateStatus={dateTimePickerError ? 'error' : ''} help={dateTimePickerError || ''}>
                 {getFieldDecorator('dateTimePicker', {
                     rules: datePickerRule
-                })(
-                    <DatePicker showTime format="YYYY-MM-DD HH:mm:ss"/>
-                )}
+                })(<DatePicker showTime format={dateTimeFormat} />)}
             </Form.Item>
 
             {/* time picker */}
-            <Form.Item label="TimePicker" validateStatus={timePickerError ? 'error' : ''}
-                       help={timePickerError || ''}>
+            <Form.Item label="TimePicker" validateStatus={timePickerError ? 'error' : ''} help={timePickerError || ''}>
                 {getFieldDecorator('timePicker', {
                     rules: timePickerRule
-                })(
-                    <TimePicker/>
-                )}
+                })(<TimePicker />)}
             </Form.Item>
 
             {/* submit button */}
@@ -126,7 +112,6 @@ const DateTimePickers: React.FC<FormComponentProps> = ({ form }: FormComponentPr
                     Submit
                 </Button>
             </Form.Item>
-
         </Form>
     );
 };
